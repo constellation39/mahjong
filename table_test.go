f2159e3c04ab8bd65e0ceb35e7b623f9269d5c52
huangjia <Constellation39@outlook.com>
2022-04-11 18:34:54 +0800

    update

--- table_test.go
+++ table_test.go
@@ -12,14 +12,13 @@ func TestDistanceEnum(t *testing.T) {
 
 func TestQuantityEnum(t *testing.T) {
 	for i := 2; i <= 14; i += 3 {
-		ret := QuantityEnum(i)
+		ret := EnumQuantity(i)
 		log.Printf("%+v", ret)
 		break
 	}
 }
 
 func Test_distanceEnum(t *testing.T) {
-
 }
 
 func Test_quantityEnum(t *testing.T) {
@@ -31,20 +30,13 @@ func PrintTiles(tiles uint64) {
 }
 
 func TestTilesEnum(t *testing.T) {
-	ret := make(map[uint64]struct{})
 	cnt := 0
 	now := time.Now()
-	for i := 2; i <= 14; i += 3 {
+	for i := 2; i <= 5; i += 3 {
 		for _, tiles := range TilesEnum(i) {
-			if _, ok := ret[tiles]; !ok {
-				cnt++
-				ret[tiles] = struct{}{}
-			}
+			log.Printf("%b", tiles)
 		}
 		log.Printf("i %d cnt %d time %s", i, cnt, time.Now().Sub(now))
 	}
 
-	for tiles := range ret {
-		GetInfo(tiles)
-	}
 }
