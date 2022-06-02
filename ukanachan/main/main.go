7550ed45d0cb1d72ec083ea893223371ce204d1f
huangjia <Constellation39@outlook.com>
2022-06-02 09:48:40 +0800

    add sub project

---
+++ ukanachan/main/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"time"
+	_ "ukanachan/paipu"
+)
+
+func main() {
+	time.Sleep(time.Second * 100)
+}
---
+++ ukanachan/paipu/paipu.go
@@ -0,0 +1,12 @@
+package paipu
+
+// func init() {
+// 	c := make(chan os.Signal, 1)
+// 	signal.Notify(c, os.Interrupt, os.Kill)
+
+// 	go func() {
+// 		for v := range c {
+// 			log.Printf("%+v", v)
+// 		}
+// 	}()
+// }
