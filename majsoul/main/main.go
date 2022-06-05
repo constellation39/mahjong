351acc5ea7af7ee8f02a03f1c16e5993674b254f
Constellation39 <Constellation39@Outlook.com>
2022-06-06 00:36:01 +0800

    update

--- majsoul/main/main.go
+++ majsoul/main/main.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	"go.uber.org/zap"
-	"majsoul/client"
+	"majsoul"
 	"utils/config"
 	"utils/logger"
 )
 
 func main() {
-	cfg := new(client.Config)
+	cfg := new(majsoul.Config)
 	err := config.Read("majsoul.json", cfg)
 	if err != nil {
 		logger.Panic("init client fail", zap.Error(err))
 	}
 
-	majsoul := client.New(cfg)
+	m := majsoul.New(cfg)
 
-	version := majsoul.GetVersion()
+	version := m.GetVersion()
 	if version.Version != "0.10.105.w" {
-		logger.Info("雀魂当前版本为", zap.String("Version", version.Version))
+		logger.Info("liqi.json的版本为0.10.105.w,雀魂当前版本为", zap.String("Version", version.Version))
 	}
 
 }
--- majsoul/majsoul.go
+++ majsoul/majsoul.go
@@ -1,4 +1,65 @@
 package majsoul
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"majsoul/message"
+	"math/rand"
+	"strings"
+	"utils/logger"
+	"utils/net"
+)
+
+type Config struct {
+	ServerAddress string `json:"serverAddress,omitempty"`
+}
+
+type ClientConn struct {
+}
+
+func (c ClientConn) Invoke(ctx context.Context, method string, args interface{}, reply interface{}, opts ...grpc.CallOption) error {
+	tokens := strings.Split("/lq.Lobby/login", "/")
+	api := strings.Join(tokens, ".")
+
+}
+
+func (c ClientConn) NewStream(ctx context.Context, desc *grpc.StreamDesc, method string, opts ...grpc.CallOption) (grpc.ClientStream, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+var cConn = &ClientConn{}
+
 type Majsoul struct {
+	request     *net.Request
+	lobbyClient message.LobbyClient
+}
+
+func New(c *Config) *Majsoul {
+	return &Majsoul{
+		request:     net.NewRequest(c.ServerAddress),
+		lobbyClient: message.NewLobbyClient(cConn),
+	}
+}
+
+type Version struct {
+	Version      string `json:"version"`
+	ForceVersion string `json:"force_version"`
+	Code         string `json:"code"`
+}
+
+func (majsoul *Majsoul) GetVersion() *Version {
+	body, err := majsoul.request.Get(fmt.Sprintf("1/version.json?randv=%d", rand.Intn(1000000000)))
+	if err != nil {
+		logger.Error("GetVersion", zap.Error(err))
+	}
+	version := new(Version)
+	err = json.Unmarshal(body, version)
+	if err != nil {
+		logger.Error("GetVersion", zap.Error(err))
+	}
+	return version
 }
